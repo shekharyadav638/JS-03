b81ba34aa7011a003aaea7b01ea46df3a2028e86
Shekhar <shekharyadav638@gmail.com>
2024-11-05 11:54:47 +0530

    Task3:Searching done through filter fucntion in JS

--- Task3/script.js
+++ Task3/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let products = [];
   let currentIndex = 0;
+  let products = [];
   const itemsPerLoad = 4;
   const loadingIcon = document.createElement("div");
   loadingIcon.className = "loading-icon";
@@ -12,28 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
         "https://fakestoreapiserver.reactbd.com/products"
       );
       products = await response.json();
-      renderProducts();
+      renderProducts(products);
     } catch (error) {
       console.error("Error fetching products:", error);
     }
   }
 
-  function renderProducts() {
+  function renderProducts(filterProducts) {
     const container = document.getElementById("list");
     const fragment = document.createDocumentFragment();
 
     for (
       let i = currentIndex;
-      i < currentIndex + itemsPerLoad && i < products.length;
+      i < currentIndex + itemsPerLoad && i < filterProducts.length;
       i++
     ) {
-      const product = products[i];
+      const product = filterProducts[i];
       const card = document.createElement("div");
       card.className = "card";
       card.innerHTML = `
-                  <img class="image" src="${product.image}" alt="${product.title}">
-                  <h2 class="title">${product.title}</h2>
-              `;
+                    <img class="image" src="${product.image}" alt="${product.title}">
+                    <h2 class="title">${product.title}</h2>
+                `;
       fragment.appendChild(card);
     }
 
@@ -56,13 +56,33 @@ document.addEventListener("DOMContentLoaded", () => {
         hideLoadingIcon();
         return;
       }
-
       showLoadingIcon();
       setTimeout(() => {
         hideLoadingIcon();
-        renderProducts();
+        const searchInput = document.getElementById("search");
+        if (searchInput.value.length === 0) renderProducts(products);
+        else filterPro();
       }, 1000);
     }
   });
   fetchProducts();
+
+  const searchInput = document.getElementById("search");
+  function filterPro() {
+    const searchValue = searchInput.value.toLowerCase();
+    if (searchValue === "") {
+      renderProducts(products);
+      return;
+    }
+    const filteredProducts = products.filter((product) =>
+      product.title.toLowerCase().includes(searchValue)
+    );
+    currentIndex = 0;
+    const container = document.getElementById("list");
+    container.innerHTML = "";
+    renderProducts(filteredProducts);
+  }
+  searchInput.addEventListener("keypress", () => {
+    filterPro();
+  });
 });
