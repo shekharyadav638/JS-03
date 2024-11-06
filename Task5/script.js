afdfaa9c09f0bd2ce4903242d436e2262a9e953d
Shekhar <shekharyadav638@gmail.com>
2024-11-06 15:58:17 +0530

    new file , new folder , collapse button added

--- Task5/script.js
+++ Task5/script.js
@@ -1,4 +1,50 @@
 const sidebar = document.getElementById("sidebar");
+const newFileButton = document.getElementById("new-file");
+const newFolderButton = document.getElementById("new-folder");
+let latestExpandedFolder = sidebar;
+
+const collapseAllButton = document.getElementById("collapse-all");
+
+collapseAllButton.addEventListener("click", () => {
+  const subFolders = document.querySelectorAll(".sub-folder");
+  subFolders.forEach((folder) => {
+    folder.style.display = "none";
+  });
+});
+
+function createInputBox(placeholder, onEnter) {
+  const existingInput = sidebar.querySelector(".name-input");
+  if (existingInput) {
+    existingInput.remove();
+    return;
+  }
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.placeholder = placeholder;
+  nameInput.className = "name-input";
+  nameInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      onEnter(nameInput.value);
+      nameInput.remove();
+    }
+  });
+  latestExpandedFolder.prepend(nameInput);
+}
+
+newFileButton.addEventListener("click", () => {
+  createInputBox("Enter the name of the file", (fileName) => {
+    addFile({ name: fileName, type: "file" }, latestExpandedFolder);
+  });
+});
+
+newFolderButton.addEventListener("click", () => {
+  createInputBox("Enter the name of the folder", (folderName) => {
+    addFolder(
+      { name: folderName, type: "folder", items: [] },
+      latestExpandedFolder
+    );
+  });
+});
 
 function addFolder(folder, parent) {
   const newFolder = document.createElement("div");
@@ -21,8 +67,11 @@ function addFolder(folder, parent) {
 
   folderTitle.addEventListener("click", (e) => {
     e.stopPropagation();
-    subFolderContainer.style.display =
-      subFolderContainer.style.display === "none" ? "block" : "none";
+    const isExpanded = subFolderContainer.style.display === "none";
+    subFolderContainer.style.display = isExpanded ? "block" : "none";
+    if (isExpanded) {
+      latestExpandedFolder = subFolderContainer;
+    }
   });
 
   parent.appendChild(newFolder);
@@ -31,7 +80,9 @@ function addFolder(folder, parent) {
 function addFile(file, parent) {
   const newFile = document.createElement("div");
   newFile.className = "file";
-  newFile.innerHTML = `<img src = "./icons/html.png" class="icon" /><span>${file.name}</span>`;
+  const fileExtension = file.name.split(".").pop();
+  const iconSrc = `./icons/${fileExtension}.png`;
+  newFile.innerHTML = `<img src="${iconSrc}" class="icon"  onerror="this.onerror=null; this.src='./icons/txt.png';"/><span>${file.name}</span>`;
   parent.appendChild(newFile);
 }
 
