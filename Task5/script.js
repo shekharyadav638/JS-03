ad0d57c73151d510caaca95248744060f2c01626
Shekhar <shekharyadav638@gmail.com>
2024-11-06 18:41:47 +0530

    Reload functionality added

--- Task5/script.js
+++ Task5/script.js
@@ -1,9 +1,10 @@
 const sidebar = document.getElementById("sidebar");
 const newFileButton = document.getElementById("new-file");
 const newFolderButton = document.getElementById("new-folder");
+const toggleSidebarButton = document.getElementById("toggle-sidebar");
+const collapseAllButton = document.getElementById("collapse-all");
 let latestExpandedFolder = sidebar;
 
-const toggleSidebarButton = document.getElementById("toggle-sidebar");
 toggleSidebarButton.addEventListener("click", () => {
   sidebar.classList.toggle("collapsed");
 
@@ -12,7 +13,6 @@ toggleSidebarButton.addEventListener("click", () => {
   icon.classList.toggle("fa-angle-up");
 });
 
-const collapseAllButton = document.getElementById("collapse-all");
 collapseAllButton.addEventListener("click", () => {
   const subFolders = document.querySelectorAll(".sub-folder");
   subFolders.forEach((folder) => {
@@ -34,6 +34,7 @@ function createInputBox(placeholder, onEnter) {
     if (e.key === "Enter") {
       onEnter(nameInput.value);
       nameInput.remove();
+      saveDataToLocalStorage();
     }
   });
   latestExpandedFolder.prepend(nameInput);
@@ -90,17 +91,50 @@ function addFile(file, parent) {
   newFile.className = "file";
   const fileExtension = file.name.split(".").pop();
   const iconSrc = `./icons/${fileExtension}.png`;
-  newFile.innerHTML = `<img src="${iconSrc}" class="icon"  onerror="this.onerror=null; this.src='./icons/txt.png';"/><span>${file.name}</span>`;
+  newFile.innerHTML = `<img src="${iconSrc}" class="icon" onerror="this.onerror=null; this.src='./icons/txt.png';"/><span>${file.name}</span>`;
   parent.appendChild(newFile);
 }
 
+function saveDataToLocalStorage() {
+  const fileStructure = buildFileStructure(sidebar);
+  localStorage.setItem("fileStructure", JSON.stringify(fileStructure));
+}
+
+function buildFileStructure(parentElement) {
+  const items = [];
+  Array.from(parentElement.children).forEach((child) => {
+    if (child.classList.contains("folder")) {
+      const folder = {
+        name: child.querySelector(".folder-header span").innerText,
+        type: "folder",
+        items: buildFileStructure(child.querySelector(".sub-folder")),
+      };
+      items.push(folder);
+    } else if (child.classList.contains("file")) {
+      const fileName = child.querySelector("span").innerText;
+      items.push({ name: fileName, type: "file" });
+    }
+  });
+  return items;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("data.json")
-    .then((response) => response.json())
-    .then((data) => {
-      data.root.forEach((item) => {
-        if (item.type === "folder") addFolder(item, sidebar);
-        else addFile(item, sidebar);
-      });
+  const savedData = localStorage.getItem("fileStructure");
+
+  if (savedData) {
+    JSON.parse(savedData).forEach((item) => {
+      if (item.type === "folder") addFolder(item, sidebar);
+      else addFile(item, sidebar);
     });
+  } else {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        data.root.forEach((item) => {
+          if (item.type === "folder") addFolder(item, sidebar);
+          else addFile(item, sidebar);
+        });
+        localStorage.setItem("fileStructure", JSON.stringify(data.root));
+      });
+  }
 });
